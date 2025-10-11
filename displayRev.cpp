d332f43a9f0823df6b8f6865b4425d1a9ee67c67
vishwas <vishwas3358b@gmail.com>
2025-10-11 18:04:07 +0530

    add a few more function

--- displayRev.cpp
+++ displayRev.cpp
@@ -2,7 +2,7 @@
 #include<stack>
 using namespace std;
  
-//function for printng the stack recursively.....
+// //function for printng the stack recursively.....
 
 void displayRev(stack<int>&st){
     // base case
@@ -14,6 +14,30 @@ void displayRev(stack<int>&st){
     st.push(x);
 }
 
+//function for putting the element at the bottom recursively......
+
+   void pustBottomRev(stack<int>&st,int val){
+         if(st.size()==0){
+            st.push(val);
+            return;
+         }
+         int x=st.top();
+         st.pop();
+         pustBottomRev(st,val);
+         st.push(x);
+
+   }
+
+   // function for revese the stack...
+   void reverse(stack<int> &st){
+    if(st.size()<=1) return;
+    int x=st.top();
+    st.pop();
+    reverse(st);
+    pustBottomRev(st,x);
+
+   }
+
 int main(){
     stack<int>st;
     st.push(10);
@@ -22,6 +46,14 @@ int main(){
     st.push(40);
     st.push(50);
     cout<<st.size()<<endl;
+    displayRev(st);
+    cout<<endl;
+    pustBottomRev(st,80);
+    cout<<endl;
+    displayRev(st);
+    cout<<endl;
+    reverse(st);
+    cout<<endl;
     displayRev(st);
    
     
