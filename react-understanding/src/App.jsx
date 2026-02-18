057ff8170ac54ac28b030b00b957c813a1dfe1da
vishwas <vishwas3358b@gmail.com>
2026-02-18 19:15:47 +0530

    add files

---
+++ react-understanding/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+// this is the functional component
+
+function App() {
+  
+  return (
+     <h1>hello vishwas</h1>
+     
+  )
+}
+
+export default App
